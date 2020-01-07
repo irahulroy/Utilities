4fd983d7184ba401ae0a2cebc551da60ebcc6254
Rahul Roy <rahulroynit@gmail.com>
2020-01-07 17:14:51 +0000

    Update Utilities.R

--- Utilities.R
+++ Utilities.R
@@ -18,8 +18,10 @@ na_values <- function(data){
   
   # create data frame containing variable names, NA value count and proportion
   # save the data frame to global environment
-  na_vals <<- data.frame(variable = colnames(na_vals), na_val = t(na_vals), 
-                         prop_na = t(100*na_vals/nrow(data)), row.names = NULL)
+  na_vals <<- data.frame(variable = colnames(na_vals), 
+                         na_val = t(na_vals), 
+                         prop_na = t(100*na_vals/nrow(data)), 
+                         row.names = NULL)
 }
 
 ## --------------------------------------------------------------------------------------------------
